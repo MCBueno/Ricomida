8beebb9687c924f5e895172be4af45b2258b24d6
MCBueno <Matiascristobalbueno@gmail.com>
2024-04-12 12:44:45 -0400

    correccion - jquery agregado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,26 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+$(document).ready(function(){
+    
+
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...");
+    });
+
+    $("#ingredientes").on("dblclick", function(){
+        $(this).css({"color":"red"})
+    });
+    $("#preparacion").on("dblclick", function(){
+        $(this).css({"color":"red"})
+    });
+
+    $(".card-title").click(function(){
+        $(".card-text").toggle();
+    });
+
+
+    $( function() {
+        $( document ).tooltip();
+    } );
+});
